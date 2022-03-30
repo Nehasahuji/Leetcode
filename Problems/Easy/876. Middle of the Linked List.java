0b6699ea94f7247462c6104d7fe5daf932258578
Neha Sahu <nehasahu9424@gmail.com>
2022-03-30 23:30:35 +0530

    added a naive solution to the problem

--- Problems/Easy/876. Middle of the Linked List.java
+++ Problems/Easy/876. Middle of the Linked List.java
@@ -1,5 +1,27 @@
 //876. Middle of the Linked List
+//naive solution
+  int count=0;
+        ListNode n = head;
+        ListNode m=head;
+        while(n.next!=null){
+            n=n.next;
+            count++;
+        }
+        
+        // System.out.println(count);
+        
+       for(int i=0;i<count/2;i++){
+           m=m.next;
+       }
+        
+        if(count%2==0)
+            return m;
+        else
+            return m.next;
+    }
+}
 
+//efficient solution
  public ListNode middleNode(ListNode head) {
         
         ListNode fast=head;
